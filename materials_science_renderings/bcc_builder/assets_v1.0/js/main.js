3a7372cbd0731ba1fdbdd04b3e38424611b14999
Sean Corcoran <sgc@vt.edu>
2025-01-31 16:22:58 -0500

    "clean up"

--- materials_science_renderings/bcc_builder/assets_v1.0/js/main.js
+++
@@ -1,9 +0,0 @@
-
-import { initBCCRender } from './bcc-render.js';
-
-// Initialize the BCC render
-document.addEventListener('DOMContentLoaded', async () => {
-  const { initBCCRender } = await import('./bcc-render.js');
-  initBCCRender('bcc-render-output');
-
-});
