1b0b61132cf7395ceaaa006a7929e81aec8d8d0d
Sean Corcoran <sgc@vt.edu>
2025-02-01 10:58:05 -0500

    live_site file system cleanup

--- live_site/ABC packing/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// main.js
-
-// Initialize the ABC render
-document.addEventListener('DOMContentLoaded', async () => {
-  const { initABCScene } = await import('./ABC.js');
-  initABCScene('ABC');
-});
--- live_site/bcc_builder/assets/js/laytex_format.js
+++
@@ -1,9 +0,0 @@
-// this .js file is placed under the <body> tag so that this redefine is defined before any latex commands are used.  <script src="assets/js/latex_format.js"></script>
-MathJax = {
-    tex: {
-      macros: {
-          oldvec: "\\vec",
-          vec: ["\\mathbf{#1}", 1]  // Redefine \vec to be \mathbf
-      }
-    }
-  };
