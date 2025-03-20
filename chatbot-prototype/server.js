3caa50ac8dfb9e0ac42e00e2ec4fb6997aa9c1ce
Sean Corcoran <sgc@vt.edu>
2025-03-19 21:18:27 -0400

    fix render issues

--- chatbot-prototype/server.js
+++ chatbot-prototype/server.js
@@ -4,8 +4,16 @@ const cors = require("cors");
 const bodyParser = require("body-parser"); 
  
 const app = express(); 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 app.use(cors()); 
-app.use(bodyParser.json()); 
+app.use(express.json()); // Ensures Express parses JSON requests
+app.use(bodyParser.urlencoded({ extended: true })); // Parses URL-encoded data
  
 const PORT = process.env.PORT || 5000; 
  
